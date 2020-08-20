813c291d5ab50047a21e7947f7c6b14f9bd866dc
Daniel Frías Ruiz <66998289+daniel-yupiketing@users.noreply.github.com>
2020-08-20 19:25:12 +0200

    Add file type

--- yk-scan-files.php
+++ yk-scan-files.php
@@ -11,31 +11,34 @@
  * Tested up to: 5.4.1
  *
  * Text Domain: yk-scan-files
- * Domain Path: /languages/
+ * Domain Path: /lang/
  */
 
 	defined( 'ABSPATH' );
 
-	function yk_scan_files_function($search, $path, $config ) {
+	function yk_scan_files_function( $search, $path, $config ) {
 		$result = '';
-		$contents = scandir($path);
-		array_splice($contents, 0,2);
+		$contents = scandir( $path );
+		array_splice( $contents, 0, 2 );
 		foreach ( $contents as $item ) {
 			$file = $path.'/'.$item;
-			if ( is_file ($file) ){
+			if ( is_file ( $file ) ){
 				$line_found = 0;
-				$lines = file($file);
-				foreach ($lines as $lineNumber => $line) {
-					if (strpos($line, $search) !== false) {
-							$line_found = $lineNumber;
+				$lines = file( $file );
+				foreach ( $lines as $lineNumber => $line ) {
+					if ( strpos( $line, $search ) !== false && !$config["file_type"] ) {
+						$line_found = $lineNumber;
+					}
+					if ( strpos( $line, $search ) !== false && $config["file_type"] && strtolower( end( explode( '.', $file ) ) ) == $config["file_type"] ) {
+						$line_found = $lineNumber;
 					}
 				}
 				if ( $line_found ){
 					$result .= '<li>'.str_replace($config["initial_path"], '', $file).' ('.__( "Line", "yk-scan-files" ).': '.$line_found.')</li>';
 				}
 			}
-			if ( is_dir($file) && (substr($item, 0,1) != '.') ) {
-				$result .= yk_scan_files_function($search, $file, $config );
+			if ( is_dir( $file ) && ( substr( $item, 0, 1 ) != '.' ) ) {
+				$result .= yk_scan_files_function( $search, $file, $config );
 			}
 		}
 		return $result;
@@ -47,15 +50,16 @@
 			<p>
 			<form method="post" action="">
 				<input type="hidden" name="action" value="scan">
-				<input type="text" name="path" value="'.$_POST["path"].'" placeholder="'.__('Path', 'yk-scan-files').'">
+				<input type="text" name="path" value="'.$_POST["path"].'" placeholder="'.__('Path', 'yk-scan-files').'" required>
 				<input type="text" name="string" value="'.$_POST["string"].'" placeholder="'.__('String', 'yk-scan-files').'" required>
+				<input type="text" name="file_type" value="'.$_POST["file_type"].'" placeholder="'.__('File type', 'yk-scan-files').'">
 				<input type="submit" class="button button-primary" value="'.__('Search', 'yk-scan-files').'">
 			</form>
 			</p>';
 			if ( $_POST["action"] == 'scan'){
 				$config = array(
 					"initial_path" 	=> '../'.$_POST["path"].'/',
-					"file_type" 		=> '',
+					"file_type" 		=> strtolower( $_POST["file_type"] ),
 				);
 				$result = yk_scan_files_function($_POST["string"], '../'.$_POST["path"], $config );
 				echo '<h2>'.__( "Results", "yk-scan-files" ).'</h2>';
